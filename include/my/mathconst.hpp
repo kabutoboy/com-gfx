a62579043c646b070179d69c14e15ec095238a46
Woratham Kh <kabuto.boy@gmail.com>
2017-02-01 02:30:18 +0700

    manual math const

--- include/my/mathconst.hpp
+++ include/my/mathconst.hpp
@@ -3,7 +3,7 @@
 
 #include <cmath>
 
-constexpr float PI = acos(-1);
-constexpr float TAU = 2*PI;
+constexpr float PI = 3.14159265359f; //acos(-1);
+constexpr float TAU = 6.28318530718f; //2*PI;
 
 #endif // ifndef MATH_CONST_
