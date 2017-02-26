4df678ff08a7a9e2baa7e2775e8da1f2095b4241
Woratham Kh <kabuto.boy@gmail.com>
2017-02-27 01:28:11 +0700

    add intuitive math const

--- include/my/mathconst.hpp
+++ include/my/mathconst.hpp
@@ -6,4 +6,8 @@
 constexpr float PI = 3.14159265359f; //acos(-1);
 constexpr float TAU = 6.28318530718f; //2*PI;
 
+constexpr float WHOLE_CIRCLE = TAU;
+constexpr float HALF_CIRCLE = TAU / 2.0f;
+constexpr float QUARTER_CIRCLE = TAU / 4.0f;
+
 #endif // ifndef MATH_CONST_
